dcd3efeaf538df257122ecbf5bda6e8b4d1a59a6
Joppe Hoekstra <joppehoekstra@Joppes-MacBook-Pro.local>
2021-08-09 12:01:28 +0200

    First commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,105 @@
+<template>
+  <q-layout view="hHr lpR fFr" class="">
+    <q-header class="bg-secondary">
+      <q-toolbar>
+
+        <q-toolbar-title>
+          Activist Handbook
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="mdi-menu" @click="toggleRightDrawer">
+          <q-tooltip>Menu</q-tooltip>
+        </q-btn>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="rightDrawerOpen" side="right" overlay behavior="mobile" elevated>
+        <q-list>
+          <q-item-label header>Activist Handbook</q-item-label>
+
+          <q-item active clickable v-ripple class="text-bold">
+            <q-item-section avatar>
+              <q-icon name="mdi-home" />
+            </q-item-section>
+
+            <q-item-section>
+              Home
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple class="text-bold">
+            <q-item-section avatar>
+              <q-icon name="mdi-calendar"/>
+            </q-item-section>
+
+            <q-item-section>
+              Events
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="mdi-account-group"/>
+            </q-item-section>
+
+            <q-item-section class="text-bold">
+              Community
+            </q-item-section>
+          </q-item>
+        </q-list>
+        <!-- <div class="q-mx-md q-mt-lg">
+          <h2 class="q-mb-md">Already a member?</h2>
+          <p>Access the dashboard with your account:</p>
+          <q-btn label="Log in" color="primary"/>
+        </div> -->
+        <div class="q-mx-md q-mt-lg">
+          <h2 class="q-mb-md">Welcome back, Joppe!</h2>
+          <p>Open the dashboard to manage your movement:</p>
+          <div class="q-gutter-sm">
+            <q-btn label="Dashboard" icon="mdi-view-dashboard" color="primary"/>
+            <!-- <q-btn label="Admin" icon="mdi-database-cog" color="primary"/> -->
+            <q-btn label="Sign out" color="primary" outline/>
+          </div>
+
+        </div>
+
+    </q-drawer>
+
+    <q-page-container class="flex justify-center">
+      <router-view />
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+            <q-fab
+              icon="mdi-chat"
+              color="secondary"
+            >
+
+            </q-fab>
+          </q-page-sticky>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup () {
+    const rightDrawerOpen = ref(false)
+
+    return {
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+<style>
+.q-page{
+  max-width: 640px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { route } from 'quasar/wrappers'
+import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import routes from './routes'
+
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Router instance.
+ */
+
+// DYNAMICALLY CHANGE COLOURS
+import { colors, setCssVar } from 'quasar'
+const { getPaletteColor } = colors
+
+setCssVar('primary', '#0431EA')
+setCssVar('secondary', '#D70057')
+setCssVar('accent', '#FFF8DB')
+setCssVar('dark', '#000000')
+console.log(getPaletteColor('light'))
+
+export default route(function (/* { store, ssrContext } */) {
+  const createHistory = process.env.SERVER
+    ? createMemoryHistory
+    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+
+  const Router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes,
+
+    // Leave this as is and make changes in quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
+  })
+
+  return Router
+})
