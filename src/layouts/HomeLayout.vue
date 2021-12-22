d5a280b872a9e717d07d6213b5a7abac44f9a8a5
Joppe Hoekstra <joppehoekstra@icloud.com>
2021-12-22 11:10:01 +0100

    Add header to home layout

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -5,15 +5,39 @@ This layout is used for public pages meant to motivate people to start using Reb
 Path: /*
  -->
 <template>
-  <q-layout view="hHh lpR fFf" >
+  <q-layout view="hHh lpR fFf">
+
+    <q-header bordered class="bg-white text-black non-selectable">
+      <q-toolbar>
+          <q-avatar square @click="$router.push({name: 'Home'})" class="cursor-pointer">
+            <img src="../assets/logo.svg">
+          </q-avatar>
+        <q-toolbar-title @click="$router.push({name: 'Home'})" class="cursor-pointer">
+          Rebel Tools
+        </q-toolbar-title>
+        <q-btn icon="mdi-github" label="Contribute" outline no-caps @click="openExternal('https://github.com/activisthandbook/rebeltools')"/>
+      </q-toolbar>
+    </q-header>
+
     <q-page-container class="flex justify-center">
       <q-page padding class="q-pt-lg q-pb-xl full-width">
         <router-view />
       </q-page>
     </q-page-container>
+
   </q-layout>
 </template>
+<script>
+import { openURL } from 'quasar'
 
+export default {
+  methods: {
+    openExternal: (link) => {
+      openURL(link)
+    }
+  }
+}
+</script>
 <style scoped>
 .q-page{
   max-width: 640px;
