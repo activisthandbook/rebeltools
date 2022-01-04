1475a7a13f2aa7db3938cf8538051410e21c4ffa
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-01-04 15:11:20 +0100

    Remove then

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -98,15 +98,11 @@ export default boot(async ({ store }) => {
 
     /* Optional argument. If true, the SDK automatically refreshes App Check tokens as needed. */
     isTokenAutoRefreshEnabled: true
-  }).then(() => {
-    // succesful
   })
 
   /* 🤖 PERFORNANCE MONITORING
   Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this key is the counterpart to the secret key you set in the Firebase console.
   Docs: https://firebase.google.com/docs/app-check
   */
-  await getPerformance(app).then(() => {
-    // succesful
-  })
+  await getPerformance(app)
 })
