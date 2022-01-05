861fdbcdb07ade791450dea1dafaed2d666d4519
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-01-05 15:51:29 +0100

    Restructure content

---
+++ src/pages/campaigns/CampaignsFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="q-gutter-y-md q-mt-lg">
+    <h3>About this article</h3>
+    <div>As you may have noticed, this article is not really a 'neutral' review. <strong>We are clearly trying to convince you to use Rebel Tools. Why?</strong> Well, we really think Rebel Tools is an awesome platform for activists. We want more people to know about it. And we have noticed these kinds of articles end up high in the Google search results.</div>
+    <div>Rebel Tools is developed by the same people who built Activist Handbook, the Wikipedia for activists. Of course, we would love you to start using Rebel Tools. But if you want to look a bit further, feel free to check out our (more objectively written) articles on <a href="https://activisthandbook.org/en/tools">tools for protestors</a>.</div>
+  </section>
+</template>
