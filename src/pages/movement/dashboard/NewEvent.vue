a9720df0c406347307ac33edfecafb26ba787438
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-02-22 14:49:33 +0100

    Added fields for new event

--- src/pages/movement/dashboard/NewEvent.vue
+++ src/pages/movement/dashboard/NewEvent.vue
@@ -45,15 +45,14 @@
             </template>
           </q-input>
 
-          {{newEvent.startDate}}
-
-          <q-input label="👉 Call to action" outlined type="textarea" color="secondary" v-model="newEvent.description" autogrow input-style="min-height: 2em" counter maxlength="250" hint="Keep it short"/>
+          <q-input label="👉 Call to action" outlined type="textarea" color="secondary" v-model="newEvent.callToAction" autogrow input-style="min-height: 2em" counter maxlength="250" hint="Keep it short"/>
 
           <q-input label="📝 Event description" outlined type="textarea" color="secondary" v-model="newEvent.description"  autogrow input-style="min-height: 6em"/>
 
         </div>
       </q-card-section>
     </q-card>
+
     <q-card>
       <q-card-section>
         <div class="q-gutter-y-sm">
@@ -72,11 +71,39 @@
             outlined color="secondary"
             type="datetime-local"
             stack-label
-            v-model="newEvent.startDate"
-            :error="this.v$.newEvent.startDate.$error"
-            :errorMessage="mixin_mergeErrorMessages(this.v$.newEvent.startDate.$errors)"
+            :disable="!newEvent.startDate"
+            :dense="!newEvent.startDate"
+            v-model="newEvent.endDate"
+            :error="this.v$.newEvent.endDate.$error"
+            :errorMessage="mixin_mergeErrorMessages(this.v$.newEvent.endDate.$errors)"
           />
           <div class="text-caption">Automatically uses your timezone</div>
+        </div>
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-card-section>
+        <div class="q-gutter-y-sm">
+          <div class="text-bold">Location (optional)</div>
+          <q-input
+            label="📍 Address"
+            outlined color="secondary"
+            v-model="newEvent.address"
+            :error="this.v$.newEvent.address.$error"
+            :errorMessage="mixin_mergeErrorMessages(this.v$.newEvent.address.$errors)"
+          />
+          <q-input
+            label="🔗 Link to join online"
+            outlined color="secondary"
+            hint="Video call, livestream or other online space"
+            v-model="newEvent.onlineLink"
+            :error="this.v$.newEvent.onlineLink.$error"
+            :errorMessage="mixin_mergeErrorMessages(this.v$.newEvent.onlineLink.$errors)"
+          >
+             <template v-slot:append>
+                <q-btn icon="mdi-video-plus" label="Add video call" no-caps color="secondary" flat dense @click="generateJitsiLink()" v-show="!jitsiLinkGenerated || !newEvent.onlineLink"/>
+              </template>
+          </q-input>
         </div>
       </q-card-section>
     </q-card>
@@ -95,42 +122,80 @@
       </q-card-section>
     </q-card>
     <q-card>
-      <q-card-section>
-        <div class="q-gutter-y-sm">
-          <div class="text-bold">Magic sync ✨</div>
-          <div>Soon, you'll be able to sync your Rebel Tools events automagically.</div>
-          <q-list bordered class="rounded-borders">
-            <q-item tag="label" v-ripple disable>
-              <q-item-section avatar>
-                <q-icon name="mdi-calendar"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Google Calendar</q-item-label>
-              </q-item-section>
-              <q-item-section avatar>
-                <q-toggle color="secondary"/>
-              </q-item-section>
-            </q-item>
-            <q-item tag="label" v-ripple disable>
-              <q-item-section avatar>
-                <q-icon name="mdi-facebook"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>Facebook Event</q-item-label>
-              </q-item-section>
-              <q-item-section avatar>
-                <q-toggle color="secondary"/>
-              </q-item-section>
-            </q-item>
+      <q-expansion-item expand-separator icon="mdi-information" label="Info for participants">
+        <q-card>
+          <q-card-section>
+            <div class="q-gutter-sm">
+              <q-input
+                label="📄 Document link"
+                outlined color="secondary"
+                v-model="newEvent.document"
+                :error="this.v$.newEvent.document.$error"
+                :errorMessage="mixin_mergeErrorMessages(this.v$.newEvent.document.$errors)"
+
+              >
+                <template v-slot:append>
+                  <q-btn icon="mdi-file-document" label="Create Google Docs" no-caps color="grey" flat dense>
+                    <q-tooltip>Google Docs integration coming soon</q-tooltip>
+                  </q-btn>
+                </template>
+              </q-input>
+              <q-input label="1️⃣ How to prepare" outlined type="textarea" color="secondary" v-model="newEvent.prepare"  autogrow input-style="min-height: 4em"/>
+              <q-input label="2️⃣ Next steps" outlined type="textarea" color="secondary" v-model="newEvent.nextSteps"  autogrow input-style="min-height: 4em"/>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+      <q-expansion-item expand-separator icon="mdi-calendar-sync" label="Magic sync ✨">
+        <q-card>
+          <q-card-section>
+            <div class="q-gutter-y-sm">
+              <div>Soon, you'll be able to sync your Rebel Tools events automagically.</div>
+              <q-list bordered class="rounded-borders">
+                <q-item tag="label" v-ripple disable>
+                  <q-item-section avatar>
+                    <q-icon name="mdi-calendar"/>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Google Calendar</q-item-label>
+                  </q-item-section>
+                  <q-item-section avatar>
+                    <q-toggle color="secondary"/>
+                  </q-item-section>
+                </q-item>
+                <q-item tag="label" v-ripple disable>
+                  <q-item-section avatar>
+                    <q-icon name="mdi-facebook"/>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Facebook Event</q-item-label>
+                  </q-item-section>
+                  <q-item-section avatar>
+                    <q-toggle color="secondary"/>
+                  </q-item-section>
+                </q-item>
           </q-list>
-        </div>
-      </q-card-section>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
     </q-card>
 
+    <activist-handbook
+      title="Guides for organisers"
+      description="Learn how to organise succesful actions from experienced campaigners around the globe. Here are some relevant guides from Activist Handbook:"
+      campaign="new_event"
+      :articles="[
+        {title: 'How to organise actions', link: 'https://activisthandbook.org/en/organising/action'},
+        {title: 'Explore tactic', link: 'https://activisthandbook.org/en/tactics'}
+      ]"
+    />
   </div>
 
 </template>
 <script>
+import ActivistHandbook from 'components/ActivistHandbook'
+
 import { Timestamp, serverTimestamp } from 'firebase/firestore'
 import { getAnalytics, logEvent } from 'firebase/analytics'
 const analytics = getAnalytics()
@@ -139,17 +204,25 @@ import useVuelidate from '@vuelidate/core'
 
 export default {
   props: ['open'],
+  components: { ActivistHandbook },
   setup () {
     return { v$: useVuelidate() }
   },
   data () {
     return {
       loading: false,
+      jitsiLinkGenerated: false,
       newEvent: {
         title: '',
         path: '',
+        address: '',
+        onlineLink: '',
         startDate: '',
-        description: ''
+        callToAction: '',
+        description: '',
+        document: '',
+        prepare: '',
+        followUp: ''
       }
     }
   },
@@ -159,6 +232,16 @@ export default {
     }
   },
   methods: {
+    generateJitsiLink () {
+      let result = ''
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      const charactersLength = characters.length
+      for (let i = 0; i < 12; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+      }
+      this.newEvent.onlineLink = 'https://meet.jit.si/' + this.$store.state.currentMovement.data.path + '-' + result
+      this.jitsiLinkGenerated = true
+    },
     async createEvent () {
       this.loading = true
 
