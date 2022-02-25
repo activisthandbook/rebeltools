2d8ae7ef992bf72cecfef2e065054afa3d7acde7
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-02-25 15:36:11 +0100

    Added cloud functions

---
+++ functions/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "standard",
+  ],
+  rules: {
+    // quotes: ["error", "double"],
+  },
+};
---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions')
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.createUser = functions.firestore
+  .document('actions/{actId}')
+  .onCreate((snap, context) => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    const newValue = snap.data()
+
+    // access a particular field as you would any JS property
+    // const name = newValue.name
+
+    functions.logger.info(newValue, { structuredData: true })
+
+    // perform desired operations ...
+  })
