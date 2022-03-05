c0e3198d3ba10eddae9f95c3a1c558f193b7c781
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-03-05 16:29:41 +0100

    Add new functions

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,10 @@ admin.initializeApp();
 
 const onActionCreate = require('./firestore/onActionCreate')
 const onAuthCreate = require('./firestore/onAuthCreate')
+const onUserUpdate = require('./firestore/onUserUpdate')
+const getLocation = require('./https/getLocation')
 
 exports.onActionCreate = onActionCreate.onActionCreate
 exports.onAuthCreate = onAuthCreate.onAuthCreate
+exports.onUserUpdate = onUserUpdate.onUserUpdate
+exports.getLocation = getLocation.getLocation
