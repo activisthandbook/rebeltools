7b14661b64697010910678b4b4d8c8850b883e0d
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-03-08 16:37:15 +0100

    Remove unused animations

--- quasar.conf.js
+++ quasar.conf.js
@@ -100,7 +100,7 @@ module.exports = configure(function (ctx) {
 
     // animations: 'all', // --- includes all animations
     // https://v2.quasar.dev/options/animations
-    animations: ['fadeIn', 'bounce'],
+    animations: [],
 
     // https://v2.quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
