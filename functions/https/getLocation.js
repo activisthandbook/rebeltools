70dc292e5022e27dd77953c57020c3eced7e4557
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-03-13 13:56:21 +0100

    Update getLocation.js

--- functions/https/getLocation.js
+++ functions/https/getLocation.js
@@ -6,12 +6,19 @@ const cors = require('cors')({
 
 // https://cloud.google.com/appengine/docs/standard/go/reference/request-response-headers#app_engine-specific_headers
 exports.getLocation = functions.region('europe-west1').https.onRequest((req, res) => {
+
+  functions.logger.info('getLocation req', req)
+  functions.logger.info('getLocation res', res)
+
   if (req.method === 'PUT') {
     res.status(403).send('Forbidden!');
     return;
   }
 
   cors(req, res, () => {
+    functions.logger.info('cors req', req)
+    functions.logger.info('cors req', res)
+
     const response = {
       "city": req.headers['X-Appengine-City'],
       "country": req.headers['X-Appengine-Country'],
