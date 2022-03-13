907e31029b3df495f4449180d0a156f5c7fcedf4
Joppe Hoekstra <joppehoekstra@icloud.com>
2022-03-13 14:24:52 +0100

    Update getLocation.js

--- functions/https/getLocation.js
+++ functions/https/getLocation.js
@@ -1,31 +1,25 @@
 const functions = require('firebase-functions');
 
-const cors = require('cors')({
-  origin: true
-});
+// const cors = require('cors')({
+//   origin: true
+// });
 
 // https://cloud.google.com/appengine/docs/standard/go/reference/request-response-headers#app_engine-specific_headers
-exports.getLocation = functions.region('europe-west1').https.onRequest((req, res) => {
+exports.getLocation = functions.region('europe-west1').https.onCall((data, context) => {
 
-  functions.logger.info('getLocation req', req)
-  functions.logger.info('getLocation res', res)
+  functions.logger.info('getLocation rawRequest', context.rawRequest)
 
-  if (req.method === 'PUT') {
-    res.status(403).send('Forbidden!');
-    return;
-  }
+  return context.rawRequest.headers
 
-  cors(req, res, () => {
-    functions.logger.info('cors req', req)
-    functions.logger.info('cors req', res)
+  // cors(req, res, () => {
 
-    const response = {
-      "city": req.headers['X-Appengine-City'],
-      "country": req.headers['X-Appengine-Country'],
-      "CityLatLong": req.headers['X-Appengine-CityLatLong']
-    }
-    res.status(200).send(response);
-    functions.logger.log('Sending location:', response);
-  })
+  //   const response = {
+  //     "city": req.headers['X-Appengine-City'],
+  //     "country": req.headers['X-Appengine-Country'],
+  //     "CityLatLong": req.headers['X-Appengine-CityLatLong']
+  //   }
+  //   res.status(200).send(response);
+  //   functions.logger.log('Sending location:', response);
+  // })
 
 });
